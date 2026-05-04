fa493ec25f27d7136721b781f9d695ccb2872bf9
RAKOTONIAINA Njara Voniarinjatovo <voniarinjatovorakotoniaina@gmail.com>
2026-05-04 14:30:26 +0300

    Correction de l'URL WebSocket pour Render

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-// config.js
-const WS_URL = window.location.hostname === 'localhost' 
-    ? 'ws://localhost:3001'
-    : `wss://${window.location.hostname}`;
+const CONFIG = {
+    // Si l'URL contient 'onrender.com', on utilise l'adresse sécurisée de Render
+    // Sinon, on utilise l'adresse locale pour tes tests sur PC
+    WS_URL: window.location.hostname.includes('onrender.com') 
+        ? `wss://${window.location.hostname}` 
+        : 'ws://localhost:10000'
+};
