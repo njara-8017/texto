b896c5c821e829e0e421d704836437b19b7a0a0f
RAKOTONIAINA Njara Voniarinjatovo <voniarinjatovorakotoniaina@gmail.com>
2026-05-05 14:28:54 +0300

    Nettoyage complet du dépôt

--- config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    // Si l'URL contient 'onrender.com', on utilise l'adresse sécurisée de Render
-    // Sinon, on utilise l'adresse locale pour tes tests sur PC
-    WS_URL: window.location.hostname.includes('onrender.com') 
-        ? `wss://${window.location.hostname}` 
-        : 'ws://localhost:10000'
-};
